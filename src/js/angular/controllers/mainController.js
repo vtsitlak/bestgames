18cae1013a5a04f880d5947aeb6bcc64a47ffb95
vtsitlak <vtsitlak@gmail.com>
2016-10-09 21:38:43 +0100

    remove duplicated select language settings

--- src/js/angular/controllers/mainController.js
+++ src/js/angular/controllers/mainController.js
@@ -11,31 +11,6 @@ app.controller('mainController', ['$scope', '$state', 'gamesService', 'stateServ
     // set the maximum star rating
     $scope.maxRate = 5;
 
-    //set the language select options
-    $('#options').flagStrap({
-        countries: {
-            "US": "English (US)",
-            "FR": "Français",
-            "ES": "Español",
-            "NL": "Nederlands",
-            "DE": "Deutsch",
-            "RU": "Русский",
-            "JP": "日本語",
-            "PL": "Polski",
-            "TR": "Türkçe",
-            "IT": "Italiano",
-            "RO": "Română",
-            "PT": "Português (PT)",
-            "BR": "Português (BR)",
-            "CN": "简体中文"
-        },
-        buttonSize: "btn-sm",
-        buttonType: "btn-info",
-        labelMargin: "10px",
-        scrollable: false,
-        scrollableHeight: "350px"
-    });
-
     // set carousel settings
     $scope.myInterval = 4000;
     $scope.noWrapSlides = false;
